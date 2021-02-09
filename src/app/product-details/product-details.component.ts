d3ddb1c65a4857afcabecd3997b153a4ae7a5cc1
Justin Oberg <oberg218@morris.umn.edu>
2021-02-08 21:38:50 -0600

    Add product description capability
    Added product description and managed how it was displayed in the template.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
+
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  product;
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
 
-  constructor() { }
+  ngOnInit() {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
 
-  ngOnInit(): void {
+    // Fnd the product that corresponds with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
